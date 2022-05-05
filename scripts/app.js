3c8be2ad1e7f581b8990f739a043e759f600a3b8
Home <mymakill@gmail.com>
2022-05-05 22:27:46 +0300

    version 1.4

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,13 @@
 $(function() {
 
-// Скролл на блок при нажатии на навигацию
+    let nav = $('#nav');
+    let burger = $('.burger')
+
+
+/*Происходит отслеживание клика по дата атрибуту в блоке nav.
+Определение нахождения блока относительно топ-позиции.
+И прокрутка на эту позицию при нажатии.
+*/ 
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
 
@@ -8,13 +15,25 @@ $(function() {
         let elementOffset = $(elementId).offset().top;
 
         $("html, body").animate({
-            scrollTop: elementOffset
-        });
+            scrollTop: elementOffset -70
+        }, 700);
+
+        nav.removeClass('show');
+
+        burger.removeClass('burger_active');
     });
 
-// Нажатие на бургер меню
+// Нажатие на бургер меню для его анимации
     $('.burger').on('click', function(e) {
         e.preventDefault;
         $(this).toggleClass('burger_active')
-    })
+    });
+
+/*Нажатие на бургер => добавление класса show для блока nav. 
+Позволяет показывать и скрывать навигаю при бургер меню*/ 
+    $('#navToggle').on('click', function(e) {
+        e.preventDefault();
+
+        nav.toggleClass('show');
+    });
 });
